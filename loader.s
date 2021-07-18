2c275051c82edcad8d93b631f6716bc4f3182e07
Sadisha Nimsara <nsadisha@gmail.com>
2021-07-18 14:37:48 +0530

    stack set up

--- loader.s
+++ loader.s
@@ -10,8 +10,17 @@
         dd MAGIC_NUMBER             ; write the magic number to the machine code,
         dd FLAGS                    ; the flags,
         dd CHECKSUM                 ; and the checksum
+        
+    KERNEL_STACK_SIZE equ 4096                  ; size of stack in bytes
+
+    section .bss
+    align 4                                     ; align at 4 bytes
+    kernel_stack:                               ; label points to beginning of memory
+        resb KERNEL_STACK_SIZE                  ; reserve stack for the kernel
 
     loader:                         ; the loader label (defined as entry point in linker script)
         mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
+        mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the
+                                                ; stack (end of memory area)
     .loop:
         jmp .loop                   ; loop forever
