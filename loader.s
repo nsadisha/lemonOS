6e24acbc238cc6d9d5ef84e341b1706a37a601b0
Sadisha Nimsara <42792030+nsadisha@users.noreply.github.com>
2021-07-21 10:24:53 +0530

    Implement with c (#2)
    * loader.s created
    
    * loader.s compiled
    
    * linking the kernel
    
    * executable file created from link.ld
    
    * stage2_eltorito downloaded and added to the project
    
    * folder structure created
    
    * image file created
    
    * configuration for testing the iso in bochs
    
    * licecnse and readme deleted. will be added later
    
    * display_library is set to sdl2
    
    * log file
    
    * log file
    
    * stack set up
    
    * this file created to run all necessary commands in a single file
    
    * kmain.c created
    
    * files created by running process
    
    * function added
    
    * files created by running process
    
    * calling c function in assembly
    
    * deleted
    
    * compiled and log files

--- loader.s
+++ loader.s
@@ -1,4 +1,8 @@
     global loader                   ; the entry symbol for ELF
+<<<<<<< HEAD
+    extern sum_of_three
+=======
+>>>>>>> master
 
     MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
     FLAGS        equ 0x0            ; multiboot flags
@@ -10,8 +14,29 @@
         dd MAGIC_NUMBER             ; write the magic number to the machine code,
         dd FLAGS                    ; the flags,
         dd CHECKSUM                 ; and the checksum
+<<<<<<< HEAD
+        
+    KERNEL_STACK_SIZE equ 4096                  ; size of stack in bytes
+
+    section .bss
+    align 4                                     ; align at 4 bytes
+    kernel_stack:                               ; label points to beginning of memory
+        resb KERNEL_STACK_SIZE                  ; reserve stack for the kernel
 
     loader:                         ; the loader label (defined as entry point in linker script)
         mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
+        mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the
+                                                ; stack (end of memory area)
+    
+    push dword 3
+    push dword 2
+    push dword 1
+    call sum_of_three
+    
+=======
+
+    loader:                         ; the loader label (defined as entry point in linker script)
+        mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
+>>>>>>> master
     .loop:
         jmp .loop                   ; loop forever
