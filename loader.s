b4dff026c7282bcd0ea37c83134e595dace9621f
nsadisha <nsadisha@gmail.com>
2021-08-26 14:12:31 +0530

    added multiboot flags to kernel

--- loader.s
+++ loader.s
@@ -1,8 +1,8 @@
     global loader                   ; the entry symbol for ELF
 
     MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
-    FLAGS        equ 0x0            ; multiboot flags
-    CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
+    FLAGS        equ 0x00000001     ; multiboot flags / align modules
+    CHECKSUM     equ -(MAGIC_NUMBER + FLAGS)  ; calculate the checksum
                                     ; (magic number + checksum + flags should equal 0)
                                     
     KERNEL_STACK_SIZE equ 4096                  ; size of stack in bytes
